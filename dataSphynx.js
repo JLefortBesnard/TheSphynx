1a3a6610d5b9a1a76f51af4316b7dcde234b9fb4
Vladislav <vladislavik8@gmail.com>
2023-04-08 16:26:49 +0200

    dataSphynx

--- dataSphynx.js
+++ dataSphynx.js
@@ -7,3 +7,4 @@
 ];
 const tabArmes = ["hache","couteau de cuisine","bouteille","épée","poing américain"];
 const tabMotifs =["Jalousie","Argent","Triangle amoureux","Vengeance personnelle","vengeance d'un proche"];
+const tabLieux = ["Café","Bar","Foret","Musee","Salon","Quai","Stade","Route","Villa","Plage"];
